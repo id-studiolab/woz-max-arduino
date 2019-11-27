0bb2a3396b704d069faf8704052d049cd526d80a
Aadjan van der Helm <a.j.c.vanderhelm@tudelft.nl>
2019-11-27 22:21:12 +0100

    Fix lotus chainable led
    Remove comment in loop to allow sensordata to be send

--- lotus-chainableled/talkToMax-lotus-chainableled/talkToMax-lotus-chainableled.ino
+++ lotus-chainableled/talkToMax-lotus-chainableled/talkToMax-lotus-chainableled.ino
@@ -93,7 +93,7 @@ void loop()
   MapSensorToActuator();
 
   // Write serial data for sensors
-//  SerialWriteSensorValues();
+  SerialWriteSensorValues();
 
   // Set actuator values to ports
   SetActuators();
